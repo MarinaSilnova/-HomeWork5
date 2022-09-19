9da2942f6e9a6a849c98dd485165d7d232f6a1f3
Marina Silnova <marusia421@mail.ru>
2022-09-19 19:04:53 +0300

    add ex 34

---
+++ ex34/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+}
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        count++;
+    }
+    return count;
+}
+int[] array = new int[12];
+InputArray(array);
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+Console.WriteLine(PositiveNumber(array));
